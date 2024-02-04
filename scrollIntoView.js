fe331eb770cdb2c90d5805172aac511e6edee0bb
JordanTimberlake <u21565882@tuks.co.za>
2024-02-04 15:16:06 +0200

    commit

---
+++ scrollIntoView.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1); // Remove the "#" from the href
+        const targetSection = document.getElementById(targetId);
+
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
